645bfbf1728d1a190d4ada2171686530f12090ee
shlomo <shlomotion1234@gmail.com>
2022-06-20 11:34:55 -0400

    Update TestAmazon.java

--- src/test/java/example/TestAmazon.java
+++ src/test/java/example/TestAmazon.java
@@ -13,8 +13,6 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-
-
 public class TestAmazon {
 
 	@DataProvider(name = "data-provider")
